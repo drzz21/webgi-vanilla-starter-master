aeef690545acf7848d776c2e997505544a238f90
Francisco D <drz.y2k@gmail.com>
2023-07-13 13:54:47 -0600

    test

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,16 @@ import {
 } from 'webgi';
 import './styles.css';
 
+import gsap from 'gsap';
+
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+let globalPosition: any;
+let globalTarget: any;
+let globalViewer:any;
+
 async function setupViewer() {
 	// Initialize the viewer
 	const viewer = new ViewerApp({
@@ -37,7 +47,7 @@ async function setupViewer() {
 	const manager = await viewer.addPlugin(AssetManagerPlugin);
 
 	// Add a popup(in HTML) with download progress when any asset is downloading.
-	await viewer.addPlugin(AssetManagerBasicPopupPlugin);
+	// await viewer.addPlugin(AssetManagerBasicPopupPlugin);
 
 	// Add plugins individually.
 	await viewer.addPlugin(GBufferPlugin);
@@ -59,7 +69,7 @@ async function setupViewer() {
 	// await addBasePlugins(viewer)
 
 	// Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-	await viewer.addPlugin(CanvasSnipperPlugin);
+	// await viewer.addPlugin(CanvasSnipperPlugin);
 
 	// This must be called once after all plugins are added.
 	viewer.renderer.refreshPipeline();
@@ -72,6 +82,25 @@ async function setupViewer() {
 		plugin.config.clipBackground = true;
 	}
 
+	// viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false });
+
+	globalPosition = viewer.scene.activeCamera.position;
+	globalTarget = viewer.scene.activeCamera.target;
+    globalViewer=viewer;
+
+	viewer.scene.activeCamera.controls!.enabled = false;
+	globalPosition.set(-6.96, 0.85, 4.55);
+	globalTarget.set(0.7, -0.39, 1.54);
+
+
+	viewer.addEventListener('preFrame', () => {
+		//si debemos cambiar la posicion de la camara lo hacemos y ponemos nuestra variable bandera en false
+
+		viewer.scene.activeCamera.positionUpdated(true);
+	});
+
+	console.log(viewer);
+
 	// console.log(viewer.getPlugin(TonemapPlugin));
 
 	// Load an environment map if not set in the glb file
@@ -81,6 +110,71 @@ async function setupViewer() {
 	// const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
 	// Add plugins to the UI to see their settings.
 	// uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
+	scrollAnimate();
+}
+
+function scrollAnimate() {
+	const tl = gsap.timeline();
+
+	// empezamos la animacion de nuestro timeline
+	//enviamos como primer argumento lo que queremos modificar, en este caso es la posicion de la cámara,
+	// seguido del objeto con las propiedades que queremos modificar, en este caso son las coordenadas de la cámara
+
+	tl.to(globalPosition, {
+		// estos son los valores que cambiaremos
+		//agregamos un ternario para verificar que nos encontramos en un dispositivo movil
+		//y en caso afirmativo asignar unos valores, y en caso contrario asignar otros
+		//para que el modelo se vea bien dentro de la ventana del navegador
+        x: 0.70,
+		y: 0.28,
+		z: 3.96,
+		// creamos nuestro scrolltrigger para configurarlo como el disparador de esta animación
+		scrollTrigger: {
+			//le indicamos que la animación inicia en sound section
+			trigger: '#chido',
+			// le indicamos que queremos que inicie cuando la parte superior
+			// de la clase sound-section toque la parte inferior del viewport
+			//dicho de otro modo podría decirse que empezará la animación cuando entre en el viewport el elemento
+			//con la clase sound-section, con este comportamiento podemos eliminar el atributo start, pues ese es
+			//el comportamiento por defecto de los elementos
+			start: 'top bottom',
+			// asignamos el final de la animacion como top top
+			//es decir cuando la parte superior de nuestro elemento alcance la parte superior del viewport
+			//es decir cuando el elemento y la parte superior del viewport colisionen
+			end: 'top top',
+			// scrub nos ayuda a que la posicion del elemento se mueva con el scroll, ya sea hacia adelante o hacia atrás, podemos tambien
+			//agregar un pequeño delay a este movimiento, de modo que no se mueva de forma instantánea segun el valor del elemento
+			//sino que le tome cierta cantidad de tiempo llegar a la nueva posición del scroll, en este caso la asignamos de 2
+			//segundos
+			scrub: 2,
+			// desactivamos la renderización inmediata, al ser un elemento 3d que puede costar trabajo renderizar
+			//con esto le indicamos que no intentará renderizar la animación hasta que sea disparada
+			immediateRender: false,
+		},
+		// cuando la animación termine, queremos llamar la función onUpdate, dentro de esa fnción recordar que indicamos
+		// en nuestra variable auxiliar que el elemento se necesita actualizar, y marcamos como dirty la camara
+		//despues en nuestro event listener actualizamos la posicion de la camara y ponemos la variable de nuevo en false
+		onUpdate:()=>{
+            globalViewer.setDirty();
+        },
+		//copiamos y concatenamos para ahora modificar el target, tambien animando,
+		// aqui no debemos ejecutar de nuevo onUpdate porque basta con ejecutarlo una vez
+	}).to(globalTarget, {
+		x: 0.06,
+		y: -0.03,
+		z: 1.16,
+
+		scrollTrigger: {
+			trigger: '#chido',
+			start: 'top bottom',
+			end: 'top top',
+			scrub: 2,
+			immediateRender: false,
+		},
+		// agregamos un fadeout
+		//para la seccion de jumbotron, que se activará de igual forma cuando entremos a la sección sound,
+		//aqui pasaremos la opacidad a 0 para que gradualmente se desvanezca
+	});
 }
 
 setupViewer();
